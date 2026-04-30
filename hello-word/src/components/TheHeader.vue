63ae0feda01f126b9fb8d9cde912d9b0c88b4d38
gbsalermo <enzogabrielskull@gmail.com>
2026-04-30 16:29:28 -0300

    Adicionando o the Header e apendendo sobre Single file component

---
+++ hello-word/src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <header class="header">
+        The Header
+    </header>
+</template>
+
+<script>
+    export default{ //Aqui é onde vai ficar todas as propriedades do componentes
+        
+    }
+</script>
+
+<style>
+    .header{
+        background: black;
+        color: white;
+    }
+</style>
