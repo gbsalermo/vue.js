7254991b49b0464411a9b12a58eb079dc81bf314
gbsalermo <enzogabrielskull@gmail.com>
2026-04-30 17:32:30 -0300

    Aplicando v-for

--- hello-word/src/App.vue
+++ hello-word/src/App.vue
@@ -10,6 +10,12 @@
   <div v-else-if="acessLevel === 'Marketing'">Usuario Marketing</div>
   <div v-else>Usuario User</div>
 
+  <div v-for="(obj, index) in todos"
+      v-bind:key="obj.id"
+      class="todos-item"
+  >
+      {{index}} - {{ obj.title }}
+  </div>
   <img
 
   alt="Vue logo" src="./assets/logo.png">
@@ -29,17 +35,58 @@ export default {
   },
   data(){
     return{
-      showHeader: true,
+      /* showHeader: true,
       firstName: 'Enzo',
       lastName: 'Gabriel',
       showtName: false,
-      acessLevel: 'admin'
+      acessLevel: 'admin' */
+
+      todos: 	
+[
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  }
+]
     }
   }
 }
 </script>
 
 <style>
+
+.todos-item{
+  background: #000;
+  margin: 0 0 5px 0;
+  padding: 3px 6px;
+  color: #fff;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
