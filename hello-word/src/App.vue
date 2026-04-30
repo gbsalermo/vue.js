ede8e39080e0d205dc681ca6ec37cc52d035e8be
gbsalermo <enzogabrielskull@gmail.com>
2026-04-30 17:13:04 -0300

    Aplicando v-show, v-if, v-else-if, v-else

--- hello-word/src/App.vue
+++ hello-word/src/App.vue
@@ -1,6 +1,18 @@
 <template>
-  <TheHeader/>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <TheHeader v-if="showHeader"/>
+
+  <div v-show="showName">
+    Nome: {{ firstName }} <br>
+    Sobrenome: {{ lastName }}
+  </div>
+
+  <div v-if="acessLevel ==='admin'"> Usuario Admin</div>
+  <div v-else-if="acessLevel === 'Marketing'">Usuario Marketing</div>
+  <div v-else>Usuario User</div>
+
+  <img
+
+  alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
   
 </template>
@@ -14,6 +26,15 @@ export default {
   components: {
     HelloWorld,
       TheHeader
+  },
+  data(){
+    return{
+      showHeader: true,
+      firstName: 'Enzo',
+      lastName: 'Gabriel',
+      showtName: false,
+      acessLevel: 'admin'
+    }
   }
 }
 </script>
